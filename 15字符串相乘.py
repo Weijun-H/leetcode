5d55ed0777b6db2b5f0e008882339128baa2d13f
hwj <huangweijun1001@gmail.com>
2019-05-24 23:22:20 +0800

    字符串相乘 15/50

---
+++ 15字符串相乘.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def multiply(self, num1, num2):
+        """
+        :type num1: str
+        :type num2: str
+        :rtype: str
+        """
+        temp = 0
+        for i in enumerate(num1[::-1]):
+            temp_num2 = 0
+            for j in enumerate(num2[::-1]):
+                temp_num2 += int(j[1])*pow(10, j[0])
+            temp += int(i[1])*pow(10,i[0])*temp_num2
+        return str(temp)
+if __name__ == '__main__':
+    s = Solution()
+    print(s.multiply("12","12"))
