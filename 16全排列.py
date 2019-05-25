2eac5f04fa40843a62e7123cbaa347856821d5e6
hwj <huangweijun1001@gmail.com>
2019-05-26 00:03:30 +0800

    全排列 16/50

---
+++ 16全排列.py
@@ -0,0 +1,15 @@
+class Solution:
+    def permute(self, nums) :
+        res = []
+        def backtrack(nums, tmp):
+            if not nums:
+                res.append(tmp)
+                return
+            for i in range(len(nums)):
+                backtrack(nums[:i] + nums[i+1:], tmp + [nums[i]])
+        backtrack(nums, [])
+        return res
+
+if __name__ == '__main__':
+    s = Solution()
+    print(s.permute([1,2,3]))
