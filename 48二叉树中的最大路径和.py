6351fcc860d4c4b0fe2e11fc680eab6fbb6b85c7
hwj <huangweijun1001@gmail.com>
2019-06-29 10:20:51 +0800

    二叉树最大路径和  48/50

---
+++ 48二叉树中的最大路径和.py
@@ -0,0 +1,14 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def maxPathSum(self, root: TreeNode, ok=True) -> int:
+        if not root: return 0
+        l, r = self.maxPathSum(root.left, False), self.maxPathSum(root.right, False)
+        self.max = max(getattr(self, 'max', float('-inf')), l + root.val + r)
+        return self.max if ok else max(root.val + max(l, r), 0)
+
